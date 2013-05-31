aad5ff9186bb3e3f56b5daa5294f407e751232e6
potar <potar@quintagroup.com>
2013-05-31 16:02:41 +0300

    uncomment the code

--- collective/recipe/logger/daemon.py
+++ collective/recipe/logger/daemon.py
@@ -28,62 +28,62 @@ class Daemon(object):
         self.umask = umask
         self.daemon_alive = True
 
-#    def daemonize(self):
-#        """ Do the UNIX double-fork magic, see Stevens' "Advanced
-#        Programming in the UNIX Environment" for details (ISBN 0201563177)
-#        http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
-#        """
-#        try:
-#            pid = os.fork()
-#            if pid > 0:
-#               # Exit first parent
-#                sys.exit(0)
-#        except OSError, e:
-#            sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
-#            sys.exit(1)
-#
-#        # Decouple from parent environment
-#        os.chdir(self.home_dir)
-#        os.setsid()
-#        os.umask(self.umask)
-#
-#        # Do second fork
-#        try:
-#            pid = os.fork()
-#            if pid > 0:
-#                # Exit from second parent
-#                sys.exit(0)
-#        except OSError, e:
-#            sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
-#            sys.exit(1)
-#
-#        if sys.platform != 'darwin':    # This block breaks on OS X
-#            # Redirect standard file descriptors
-#            sys.stdout.flush()
-#            sys.stderr.flush()
-#            si = open(self.stdin, 'r')
-#            so = open(self.stdout, 'a+')
-#            if self.stderr:
-#                se = open(self.stderr, 'a+', 0)
-#            else:
-#                se = so
-#            os.dup2(si.fileno(), sys.stdin.fileno())
-#            os.dup2(so.fileno(), sys.stdout.fileno())
-#            os.dup2(se.fileno(), sys.stderr.fileno())
-#
-#        def sigtermhandler(signum, frame):
-#            self.daemon_alive = False
-#        signal.signal(signal.SIGTERM, sigtermhandler)
-#        signal.signal(signal.SIGINT, sigtermhandler)
-#
-#        if self.verbose >= 1:
-#            print "Started"
-#
-#        # Write pidfile
-#        atexit.register(self.delpid)    # Make sure pid file is removed if we quit
-#        pid = str(os.getpid())
-#        with open(self.pidfile, 'w+') as f:
-#            f.write("%s\n" % pid)
+    def daemonize(self):
+        """ Do the UNIX double-fork magic, see Stevens' "Advanced
+        Programming in the UNIX Environment" for details (ISBN 0201563177)
+        http://www.erlenstar.demon.co.uk/unix/faq_2.html#SEC16
+        """
+        try:
+            pid = os.fork()
+            if pid > 0:
+               # Exit first parent
+                sys.exit(0)
+        except OSError, e:
+            sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+
+        # Decouple from parent environment
+        os.chdir(self.home_dir)
+        os.setsid()
+        os.umask(self.umask)
+
+        # Do second fork
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # Exit from second parent
+                sys.exit(0)
+        except OSError, e:
+            sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+
+        if sys.platform != 'darwin':    # This block breaks on OS X
+            # Redirect standard file descriptors
+            sys.stdout.flush()
+            sys.stderr.flush()
+            si = open(self.stdin, 'r')
+            so = open(self.stdout, 'a+')
+            if self.stderr:
+                se = open(self.stderr, 'a+', 0)
+            else:
+                se = so
+            os.dup2(si.fileno(), sys.stdin.fileno())
+            os.dup2(so.fileno(), sys.stdout.fileno())
+            os.dup2(se.fileno(), sys.stderr.fileno())
+
+        def sigtermhandler(signum, frame):
+            self.daemon_alive = False
+        signal.signal(signal.SIGTERM, sigtermhandler)
+        signal.signal(signal.SIGINT, sigtermhandler)
+
+        if self.verbose >= 1:
+            print "Started"
+
+        # Write pidfile
+        atexit.register(self.delpid)    # Make sure pid file is removed if we quit
+        pid = str(os.getpid())
+        with open(self.pidfile, 'w+') as f:
+            f.write("%s\n" % pid)
 
     def delpid(self):
         os.remove(self.pidfile)
@@ -112,51 +112,51 @@ class Daemon(object):
         self.daemonize()
         self.run()
 
-#    def stop(self):
-#        """
-#        Stop the daemon
-#        """
-#        if self.verbose >= 1:
-#            print "Stopping..."
-#        # Get the pid from the pidfile
-#        try:
-#            pf = open(os.path.join(self.home_dir, self.pidfile), 'r')
-#            pid = int(pf.read().strip())
-#            pf.close()
-#        except IOError:
-#            pid = None
-#        except ValueError:
-#            pid = None
-#
-#        if not pid:
-#            message = "pidfile %s does not exist. Not running?\n"
-#            sys.stderr.write(message % self.pidfile)
-#
-#            # Just to be sure. A ValueError might occur if the PID file is empty but does actually exist
-#            if os.path.exists(self.pidfile):
-#                os.remove(self.pidfile)
-#
-#            return  # Not an error in a restart
-#
-#        # Try killing the daemon processlifetime
-#        try:
-#            i = 0
-#            while 1:
-#                os.kill(pid, signal.SIGTERM)
-#                time.sleep(0.1)
-#                i = i + 1
-#                if i % 10 == 0:
-#                    os.kill(pid, signal.SIGHUP)
-#        except OSError, err:
-#            err = str(err)
-#            if err.find("No such process") > 0:
-#                if os.path.exists(self.pidfile):
-#                    os.remove(self.pidfile)
-#            else:
-#                print str(err)
-#                sys.exit(1)
-#        if self.verbose >= 1:
-#            print "Stopped"
+    def stop(self):
+        """
+        Stop the daemon
+        """
+        if self.verbose >= 1:
+            print "Stopping..."
+        # Get the pid from the pidfile
+        try:
+            pf = open(os.path.join(self.home_dir, self.pidfile), 'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+        except ValueError:
+            pid = None
+
+        if not pid:
+            message = "pidfile %s does not exist. Not running?\n"
+            sys.stderr.write(message % self.pidfile)
+
+            # Just to be sure. A ValueError might occur if the PID file is empty but does actually exist
+            if os.path.exists(self.pidfile):
+                os.remove(self.pidfile)
+
+            return  # Not an error in a restart
+
+        # Try killing the daemon processlifetime
+        try:
+            i = 0
+            while 1:
+                os.kill(pid, signal.SIGTERM)
+                time.sleep(0.1)
+                i = i + 1
+                if i % 10 == 0:
+                    os.kill(pid, signal.SIGHUP)
+        except OSError, err:
+            err = str(err)
+            if err.find("No such process") > 0:
+                if os.path.exists(self.pidfile):
+                    os.remove(self.pidfile)
+            else:
+                print str(err)
+                sys.exit(1)
+        if self.verbose >= 1:
+            print "Stopped"
 
     def restart(self):
         """ Restart the daemon """
